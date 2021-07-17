09298d6836a7513dca2f37984b227591b28df715
Prashant Rana <prasran@amazon.com>
2021-07-17 14:01:48 -0500

    Initial commit

---
+++ src/test/java/com/spring/boot/springbootpostgres/SpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
